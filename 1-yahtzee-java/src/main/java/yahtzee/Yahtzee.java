58d1d648f45f58ba3b6a13dc2c9ff96598ab1384
rojoangel <rojo.angel@gmail.com>
2016-03-23 23:04:53 +0100

    enable dice indexes to roll to be passed in

--- 1-yahtzee-java/src/main/java/yahtzee/Yahtzee.java
+++ 1-yahtzee-java/src/main/java/yahtzee/Yahtzee.java
@@ -1,5 +1,8 @@
 package yahtzee;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Yahtzee {
 
     private FakeConsole console;
@@ -13,23 +16,23 @@ public class Yahtzee {
 
     public void play() {
         this.console.print("Category: Ones");
-        roll();
+        roll(1, 2, 3, 4, 5);
         this.console.print("[1] Dice to re-run:");
         this.console.print("Dice: D1:1 D2:5 D3:1 D4:2 D5:1");
     }
 
-    private void roll() {
-        int[] rolled_dice = new int[5];
-        for (int i =0; i<5 ; i++) {
-            rolled_dice[i] = this.dieRoller.roll();
+    private void roll(int... dice) {
+        Map<Integer, Integer> rolledDice = new HashMap<Integer, Integer>();
+        for (int die : dice) {
+            rolledDice.put(die, this.dieRoller.roll());
         }
-        notifyRolledDice(rolled_dice);
+        notifyRolledDice(rolledDice);
     }
 
-    private void notifyRolledDice(int[] rolled_dice) {
+    private void notifyRolledDice(Map<Integer, Integer> rolledDice) {
         StringBuilder formattedDice = new StringBuilder("Dice:");
-        for (int i = 0; i < rolled_dice.length; i++) {
-            formattedDice.append(" D").append(i + 1).append(":").append(rolled_dice[i]);
+        for (int dieId : rolledDice.keySet()) {
+            formattedDice.append(" D").append(dieId).append(":").append(rolledDice.get(dieId));
         }
         this.console.print(formattedDice.toString());
     }
