a0d7e6cb255988529efd500ada9f04c413406ca3
rojoangel <rojo.angel@gmail.com>
2016-03-23 22:50:39 +0100

    extract dice roll to its own method

--- 1-yahtzee-java/src/main/java/yahtzee/Yahtzee.java
+++ 1-yahtzee-java/src/main/java/yahtzee/Yahtzee.java
@@ -13,14 +13,17 @@ public class Yahtzee {
 
     public void play() {
         this.console.print("Category: Ones");
+        roll();
+        this.console.print("[1] Dice to re-run:");
+        this.console.print("Dice: D1:1 D2:5 D3:1 D4:2 D5:1");
+    }
+
+    private void roll() {
         int[] rolled_dice = new int[5];
         for (int i =0; i<5 ; i++) {
             rolled_dice[i] = this.dieRoller.roll();
         }
         notifyRolledDice(rolled_dice);
-        this.console.print("[1] Dice to re-run:");
-
-        this.console.print("Dice: D1:1 D2:5 D3:1 D4:2 D5:1");
     }
 
     private void notifyRolledDice(int[] rolled_dice) {
