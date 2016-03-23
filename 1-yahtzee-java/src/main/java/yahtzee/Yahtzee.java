95d570f4abbaf7f6396c3de2b27031b772e45a56
rojoangel <rojo.angel@gmail.com>
2016-03-23 22:39:47 +0100

    rename dice to rolled_dice

--- 1-yahtzee-java/src/main/java/yahtzee/Yahtzee.java
+++ 1-yahtzee-java/src/main/java/yahtzee/Yahtzee.java
@@ -13,11 +13,11 @@ public class Yahtzee {
 
     public void play() {
         this.console.print("Category: Ones");
-        int[] dice = new int[5];
+        int[] rolled_dice = new int[5];
         for (int i =0; i<5 ; i++) {
-            dice[i] = this.dieRoller.roll();
+            rolled_dice[i] = this.dieRoller.roll();
         }
-        this.console.print("Dice: D1:" + dice[0] + " D2:" + dice[1] + " D3:" + dice[2] + " D4:" + dice[3] + " D5:" + dice[4]);
+        this.console.print("Dice: D1:" + rolled_dice[0] + " D2:" + rolled_dice[1] + " D3:" + rolled_dice[2] + " D4:" + rolled_dice[3] + " D5:" + rolled_dice[4]);
         this.console.print("[1] Dice to re-run:");
 
         this.console.print("Dice: D1:1 D2:5 D3:1 D4:2 D5:1");
