f6faaa3ce56e987567c4159b6b231bdc5a5da3c2
rojoangel <rojo.angel@gmail.com>
2016-03-24 18:26:28 +0100

    remove some duplication in console notifier test

--- 1-yahtzee-java/src/test/java/yahtzee/ConsoleNotifierTest.java
+++ 1-yahtzee-java/src/test/java/yahtzee/ConsoleNotifierTest.java
@@ -1,5 +1,6 @@
 package yahtzee;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Collections;
@@ -9,6 +10,16 @@ import java.util.Map;
 import static org.junit.Assert.*;
 
 public class ConsoleNotifierTest {
+
+
+    private Console console;
+    private ConsoleNotifier consoleNotifier;
+
+    @Before
+    public void setUp() {
+        console = new FakeConsole();
+        consoleNotifier = new ConsoleNotifier(console);
+    }
 
     @Test
     public void testNotifyRolledDice() throws Exception {
@@ -20,9 +31,6 @@ public class ConsoleNotifierTest {
         rolledDice.put(Die.D4, 6);
         rolledDice.put(Die.D5 ,1);
 
-        Console console = new FakeConsole();
-        ConsoleNotifier consoleNotifier = new ConsoleNotifier(console);
-
         consoleNotifier.notifyRolledDice(rolledDice);
 
         assertEquals(Collections.singletonList("Dice: D1:2 D2:4 D3:1 D4:6 D5:1"), console.getOutput());
@@ -30,9 +38,6 @@ public class ConsoleNotifierTest {
 
     @Test
     public void testNotifyUserToIntroduceDiceToRerun() throws Exception {
-        Console console = new FakeConsole();
-        ConsoleNotifier consoleNotifier = new ConsoleNotifier(console);
-
         int rerunsSoFar= 1;
         consoleNotifier.notifyUserToIntroduceDiceToRerun(rerunsSoFar);
         assertEquals(Collections.singletonList("[2] Dice to re-run:"), console.getOutput());
