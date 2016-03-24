79b0c97947ad8b381984fa30e1c1bf225743da1f
rojoangel <rojo.angel@gmail.com>
2016-03-24 22:27:45 +0100

    rename test

--- 1-yahtzee-java/src/test/java/yahtzee/InMemoryScoresHistoryTest.java
+++ 1-yahtzee-java/src/test/java/yahtzee/InMemoryScoresHistoryTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class ScoresHistoryTest {
+public class InMemoryScoresHistoryTest {
 
     @Test
     public void testMaxScore() throws Exception {
