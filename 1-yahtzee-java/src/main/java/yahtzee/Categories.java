661e55f512ec8ed41a43e87e081ff264c15ec5ad
rojoangel <rojo.angel@gmail.com>
2016-03-25 12:10:34 +0100

    extract reruns to its own public class

--- 1-yahtzee-java/src/main/java/yahtzee/Categories.java
+++ 1-yahtzee-java/src/main/java/yahtzee/Categories.java
@@ -34,37 +34,4 @@ public class Categories {
         this.diceRoller.roll(Die.values());
         this.notifier.notifyRolledDice(this.diceRoller.lastRolledDice());
     }
-
-    public class Reruns {
-
-        private ConsoleNotifier notifier;
-        private UserInputReader userInputReader;
-        private DiceRoller diceRoller;
-
-        public Reruns(
-                ConsoleNotifier notifier,
-                UserInputReader userInputReader,
-                DiceRoller diceRoller) {
-            this.notifier = notifier;
-            this.userInputReader = userInputReader;
-            this.diceRoller = diceRoller;
-        }
-
-        public void doReruns(int numReruns) {
-            for (int rerunsSoFar = 0; rerunsSoFar < numReruns; rerunsSoFar++) {
-                this.notifier.notifyUserToIntroduceDiceToRerun(rerunsSoFar);
-                roll(obtainDiceToRoll());
-            }
-        }
-
-        private Die[] obtainDiceToRoll() {
-            InputLine inputLine = new InputLine(this.userInputReader.readLine());
-            return inputLine.diceToRoll();
-        }
-
-        private void roll(Die... dice) {
-            this.diceRoller.roll(dice);
-            this.notifier.notifyRolledDice(Categories.this.diceRoller.lastRolledDice());
-        }
-    }
 }
