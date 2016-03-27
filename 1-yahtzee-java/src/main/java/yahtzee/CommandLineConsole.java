c64d0cc9c58483aade796f8b8fb84c5440673506
trikitrok <trikitrok@gmail.com>
2016-03-27 21:07:45 +0200

    Reduce Console interface to what is needed in production code

--- 1-yahtzee-java/src/main/java/yahtzee/CommandLineConsole.java
+++ 1-yahtzee-java/src/main/java/yahtzee/CommandLineConsole.java
@@ -1,18 +1,7 @@
 package yahtzee;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class CommandLineConsole implements Console {
-
-    List<String> lines = new ArrayList<String>();
-
     public void print(String line) {
         System.out.println(line);
-        lines.add(line);
-    }
-
-    public List<String> getOutput() {
-        return lines;
     }
 }
--- 1-yahtzee-java/src/main/java/yahtzee/Console.java
+++ 1-yahtzee-java/src/main/java/yahtzee/Console.java
@@ -1,11 +1,5 @@
 package yahtzee;
 
-import java.util.List;
-
 public interface Console {
     void print(String line);
-
-    List<String> getOutput();
-
-
 }
