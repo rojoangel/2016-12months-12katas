8765f66d8e5a284fecd395059285679612514ec6
rojoangel <rojo.angel@gmail.com>
2016-03-31 02:03:31 +0200

    add another console line and make it pass

--- 2-yahtzee-php/tests/Yahtzee/YahtzeeTest.php
+++ 2-yahtzee-php/tests/Yahtzee/YahtzeeTest.php
@@ -19,7 +19,8 @@ class YahtzeeTest extends \PHPUnit_Framework_TestCase
             "Category: Ones",
             "Dice: D1:2 D2:4 D3:1 D4:6 D5:1",
             "[1] Dice to re-run:",
-            "Dice: D1:1 D2:5 D3:1 D4:2 D5:1"
+            "Dice: D1:1 D2:5 D3:1 D4:2 D5:1",
+            "[2] Dice to re-run:"
         ];
         $this->assertEquals($outputLines, $console->output);
     }
