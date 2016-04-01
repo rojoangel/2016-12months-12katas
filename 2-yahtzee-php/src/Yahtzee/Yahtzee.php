0a395264f8f144940b29cb570f1110c14551b40d
rojoangel <rojo.angel@gmail.com>
2016-04-02 00:56:30 +0200

    add yahtzee score output line and make the test pass by using output user interface

--- 2-yahtzee-php/src/Yahtzee/Yahtzee.php
+++ 2-yahtzee-php/src/Yahtzee/Yahtzee.php
@@ -11,16 +11,22 @@ class Yahtzee
     /** @var Categories */
     private $categories;
 
+    /** @var OutputUserInterface */
+    private $outputUserInterface;
+
     /**
      * @param Categories $categories
+     * @param OutputUserInterface $outputUserInterface
      */
-    public function __construct(Categories $categories)
+    public function __construct(Categories $categories, OutputUserInterface $outputUserInterface)
     {
         $this->categories = $categories;
+        $this->outputUserInterface = $outputUserInterface;
     }
     
     public function play() {
 
         $this->categories->play(self::RERUN_ATTEMPTS);
+        $this->outputUserInterface->printLine("Yahtzee score");
     }
 }
