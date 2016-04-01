753fbd1e07c9ae28f1eb67bc109b0d68530bdb6b
rojoangel <rojo.angel@gmail.com>
2016-04-02 00:07:27 +0200

    extract doReruns method

--- 2-yahtzee-php/src/Yahtzee/Yahtzee.php
+++ 2-yahtzee-php/src/Yahtzee/Yahtzee.php
@@ -33,17 +33,13 @@ class Yahtzee
             $this->diceRoller->rollAll();
             $this->printDiceLine($this->diceRoller->lastRollResult());
 
-            for ($reRunAttempt = 1; $reRunAttempt <= self::RERUN_ATTEMPTS; $reRunAttempt++) {
-                $diceToReRun = $this->userInterface->requestDiceToReRun($reRunAttempt);
-                $this->diceRoller->reRun($diceToReRun);
-                $this->userInterface->printDiceLine($this->diceRoller->lastRollResult());
-            }
+            $this->doReRuns();
 
             $this->printCategoryScore($category, $category->calculateScore($this->diceRoller->lastRollResult()));
 
         }
     }
-    
+
     /**
      * @param Category $category
      */
@@ -67,5 +63,14 @@ class Yahtzee
     private function printCategoryScore($category, $categoryScore)
     {
         $this->userInterface->printCategoryScore($category, $categoryScore);
+    }
+
+    private function doReRuns()
+    {
+        for ($reRunAttempt = 1; $reRunAttempt <= self::RERUN_ATTEMPTS; $reRunAttempt++) {
+            $diceToReRun = $this->userInterface->requestDiceToReRun($reRunAttempt);
+            $this->diceRoller->reRun($diceToReRun);
+            $this->userInterface->printDiceLine($this->diceRoller->lastRollResult());
+        }
     }
 }
