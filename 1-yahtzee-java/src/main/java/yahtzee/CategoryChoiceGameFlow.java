08a1362a8fe1f9e1d596a4b0316236361497da73
trikitrok <trikitrok@gmail.com>
2016-04-04 20:30:53 +0200

    Doing first 3 rolls

---
+++ 1-yahtzee-java/src/main/java/yahtzee/CategoryChoiceGameFlow.java
@@ -0,0 +1,26 @@
+package yahtzee;
+
+public class CategoryChoiceGameFlow implements GameFlow {
+    private final ConsoleNotifier notifier;
+    private final DiceRoller diceRoller;
+    private final ScoresHistory scoresHistory;
+    private final Reruns reruns;
+
+    public CategoryChoiceGameFlow(ConsoleNotifier notifier, DiceRoller diceRoller, ScoresHistory scoresHistory, Reruns reruns) {
+        this.notifier = notifier;
+        this.diceRoller = diceRoller;
+        this.scoresHistory = scoresHistory;
+        this.reruns = reruns;
+    }
+
+    @Override
+    public void play(int numReruns) {
+        rollAll();
+        this.reruns.doReruns(numReruns);
+    }
+
+    private void rollAll() {
+        this.diceRoller.roll(Die.values());
+        this.notifier.notifyRolledDice(this.diceRoller.lastRolledDice());
+    }
+}
---
+++ 1-yahtzee-java/src/test/java/yahtzee/Round2YahtzeeTest.java
@@ -0,0 +1,43 @@
+package yahtzee;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class Round2YahtzeeTest {
+
+    @Test
+    public void testRoundTwoGame() {
+        FakeConsole console = new FakeConsole();
+        ConsoleNotifier notifier = new ConsoleNotifier(console);
+        DieRoller dieRoller = mock(DieRoller.class);
+        when(dieRoller.roll()).thenReturn(
+            2, 4, 1, 6, 1,
+            1, 5, 2,
+            1, 5);
+        UserInputReader userInputReader = mock(UserInputReader.class);
+        when(userInputReader.readLine()).thenReturn(
+            "D1 D2 D4",
+            "D2 D4");
+        DiceRoller diceRoller = new DiceRoller(dieRoller);
+        ScoresHistory scoresHistory = new InMemoryScoresHistory();
+        Reruns reruns = new Reruns(notifier, userInputReader, diceRoller);
+        GameFlow gameFlow = new CategoryChoiceGameFlow(notifier, diceRoller, scoresHistory, reruns);
+        Yahtzee yahtzee = new Yahtzee(notifier, scoresHistory, gameFlow);
+        yahtzee.play();
+
+        List<String> outputLines = new ArrayList<String>();
+        outputLines.add("Dice: D1:2 D2:4 D3:1 D4:6 D5:1");
+        outputLines.add("[1] Dice to re-run:");
+        outputLines.add("Dice: D1:1 D2:5 D3:1 D4:2 D5:1");
+        outputLines.add("[2] Dice to re-run:");
+        outputLines.add("Dice: D1:1 D2:1 D3:1 D4:5 D5:1");
+
+        assertEquals(outputLines, console.getOutput());
+    }
+}
